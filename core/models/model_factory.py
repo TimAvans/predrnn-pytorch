33e8b178641d4e89fd908020cfb2a687f33e944b
Tim <timjanssenj@gmail.com>
2025-07-02 14:58:36 +0200

    Added a v3 version of the model with  crosslayer attention
    also added in factory for easy use

--- core/models/model_factory.py
+++ core/models/model_factory.py
@@ -1,7 +1,7 @@
 import os
 import torch
 from torch.optim import Adam
-from core.models import predrnn, predrnn_v2, action_cond_predrnn, action_cond_predrnn_v2
+from core.models import predrnn, predrnn_v2, predrnn_v3, action_cond_predrnn, action_cond_predrnn_v2
 
 class Model(object):
     def __init__(self, configs):
@@ -11,6 +11,7 @@ class Model(object):
         networks_map = {
             'predrnn': predrnn.RNN,
             'predrnn_v2': predrnn_v2.RNN,
+            'predrnn_v3': predrnn_v3.RNN,
             'action_cond_predrnn': action_cond_predrnn.RNN,
             'action_cond_predrnn_v2': action_cond_predrnn_v2.RNN,
         }
